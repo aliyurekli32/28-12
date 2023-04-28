a14929bcef5cb9f2a3ca6f59bf03b407a9ebdffe
Ali Yürekli <109242845+aliyurekli32@users.noreply.github.com>
2023-04-28 16:22:53 +0300

    Update views.py

--- user/views.py
+++ user/views.py
@@ -6,7 +6,7 @@ from rest_framework.authtoken.models import Token
 class RegisterView(CreateAPIView):
     queryset= User.objects.all()
     serializer_class = RegisterSerializer
-    
+    #create token
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
         token = Token.objects.create(user_id=response.data['id'])
