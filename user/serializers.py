57af35ec25ce37c90d39a7dd7e21c723db48c613
Ali Yürekli <109242845+aliyurekli32@users.noreply.github.com>
2023-05-04 11:42:23 +0300

    Update serializers.py

--- user/serializers.py
+++ user/serializers.py
@@ -6,7 +6,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required = True, validators=[UniqueValidator(queryset=User.objects.all())])
     password = serializers.CharField(write_only=True)
     password2 = serializers.CharField(write_only=True, required=True)
-    #user serializer
+    #user serializer with
     ...
     class Meta:
         model = User
